2a95cb9b32c2a1d1dd9c65018b6573b1e7923324
degid <sdegid@gmail.com>
2020-08-07 00:26:10 +0300

    U2 next

--- utils.py
+++ utils.py
@@ -27,8 +27,28 @@ def delSpecCh(str):
 
 
 class Image:
-    def __init__(self):
+    def __init__(self, file):
         self.BMPnums = BMPdata.getNumsBMP()
+        self.file = file
+        self.data = None
+        if self.detektImg():
+            self.readBitMap(self.data, self.tagBITMAPFILEHEADER.bfOffBits,
+                            self.tagBITMAPINFOHEADER.biWidth,
+                            self.tagBITMAPINFOHEADER.biHeight)
+
+    def detektImg(self):
+        '''
+        Reading headers and verify it's the BMP-type
+        '''
+        self.readFile()
+        self.readBMPHeaders()
+        self.tagBITMAPFILEHEADER = None
+        self.tagBITMAPINFOHEADER = None
+        return True if self.tagBITMAPFILEHEADER.bfType == b'BM' else False
+
+    def readFile(self):
+        with open(self.file, 'rb') as f:
+            self.data = bytearray(f.read())
 
     def readBitMap(self, data, bfOffBits, biWidth, biHeight):
         padBytes = (4 - (biWidth * 3) % 4) % 4
@@ -38,16 +58,24 @@ class Image:
         for line in range(biHeight):
             BitMapLine = []
             for count, indx in enumerate(range(startLine, endLine, 3)):
-                BitMapLine.append(BMPdata.RGBQUAD._make(struct.unpack_from('<BBB', data, indx)))
+                BitMapLine.append(BMPdata.RGBQUAD._make(
+                                  struct.unpack_from('<BBB', data, indx)))
             BitMapLines.append(BitMapLine)
             startLine += biWidth * 3 + padBytes
             endLine += biWidth * 3 + padBytes
         return BitMapLines
 
-    def readBMPHeaders(self, data):
-        tagBITMAPFILEHEADER = BMPdata.BITMAPFILEHEADER._make(struct.unpack_from('<2sIHHI', data, 0))
-        tagBITMAPINFOHEADER = BMPdata.BITMAPINFOHEADER._make(struct.unpack_from('<ILLHHIILLII', data, 14))
-        return (tagBITMAPFILEHEADER, tagBITMAPINFOHEADER)
+    def readBMPHeaders(self):
+        self.tagBITMAPFILEHEADER = BMPdata.BITMAPINFOHEADER._make(
+                                   struct.unpack_from('<2sIHHI',
+                                                      self.data,
+                                                      0)
+                                )
+        self.tagBITMAPINFOHEADER = BMPdata.BITMAPINFOHEADER._make(
+                                   struct.unpack_from('<ILLHHIILLII',
+                                                      self.data,
+                                                      14)
+                                )
 
     def buildImage(BitMapFileHeader, BitMapInfoHeader, BitMapLines):
         dataImg = bytearray()
@@ -70,10 +98,10 @@ class Image:
                 dataImg += struct.pack("<B", 0)
         return dataImg
 
-        def addSymbol(self, image, X, Y, sybmol, alpha):
-            BitMap = self.readBitMap(self.BMPnums[sybmol], 1, 9, 13)
+    def addSymbol(self, image, X, Y, sybmol, alpha):
+        BitMap = self.readBitMap(self.BMPnums[sybmol], 1, 9, 13)
 
-            for line, _ in enumerate(BitMap):
-                for colum, _ in enumerate(BitMap[line]):
-                    if BitMap[line][colum] != alpha:
-                        image[line+Y][colum+X] = BitMap[line][colum]
+        for line, _ in enumerate(BitMap):
+            for colum, _ in enumerate(BitMap[line]):
+                if BitMap[line][colum] != alpha:
+                    image[line+Y][colum+X] = BitMap[line][colum]
